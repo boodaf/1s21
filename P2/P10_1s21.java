fbc47c4219242599115470684f7e2a941005342f
boodaf <144178847+boodaf@users.noreply.github.com>
2023-10-20 19:31:40 -0600

    Add files via upload

---
+++ P2/P10_1s21.java
@@ -0,0 +1,20 @@
+public class P10_1s21{
+	public static void main(String[] args) {
+		String opc="a";
+		switch(opc){
+		case "a":
+			System.out.println("aqui el caso a");
+		case "b":
+			System.out.println("aqui el caso b");
+		case "c":
+			System.out.println("aqui el caso c");
+		case "d":
+			System.out.println("aqui el caso d");
+		case "e":
+			System.out.println("aqui el caso e");
+		default:
+			System.out.println("No se encuentra la opcion deseada");
+
+		}
+	}
+}
