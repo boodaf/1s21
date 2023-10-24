4a25caaba027625504a83fa707924f5022820f85
boodaf <144178847+boodaf@users.noreply.github.com>
2023-10-24 15:27:12 -0600

    Add files via upload

---
+++ P2/P2p14_1s21.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class P2p14_1s21 sumaM{
+	public static main void(String[]args){
+		Scanner=new Scanner(System.in);
+		system.out.println("Ingrese el numero de filas");
+		f=teclado.nextInt();
+		system.out.println("Ingrese el numero de columnas");
+		c=teclado.nextInt();
+
+		Int[][]MA=new int[1][0];
+		Int[][]MB=new int[1][0];
+		Int[][]MC=new int[ff][fc];
+		int ff,fc;
+		for(if=0;ff<f;ff++){
+			system.out.println("Ingrese el valor para la posicion ["+ff+"]["+fc+"]");
+			MA[ff][fc]=teclado.nextInt();
+		}
+	}	
+        for(ff=0;ff<f;ff++){
+			system.out.println("Ingrese el valor para la posicion ["+ff+"]["+fc+"]");
+			MB[ff][fc]=teclado.nextInt();
+		}
+    }
+        for(ff=0;ff<f;ff++){
+			for(fc=0,fc<c;fc++){
+				MC[ff][fc]=MA[ff][fc]-MB[ff][fc];
+		}
+    }
