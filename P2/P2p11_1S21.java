b6a805b9000c86c20ea6c8975fd962db1071ec8e
boodaf <144178847+boodaf@users.noreply.github.com>
2023-10-27 00:30:02 -0600

    Add files via upload

---
+++ P2/P2p11_1S21.java
@@ -0,0 +1,38 @@
+import java.util Scanner
+ public class P2p11_1s21{
+ 	public static void main(String [] args){
+ 		for (String cadena= args){
+ 			char operacion=cadena.charAt();
+ 			if(operacion='?'')[
+
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("h");
+        System.out.println("e");
+        Scanner teclado =new Scanner(System.in);
+        String a ="a"
+        a=teclado.nextInc();
+        switch a {
+        Case"a":
+        System.out.println("Analisis de datos");
+        break;
+         Case"b":
+        System.out.println("Instalaciones avanzadas");
+        break;
+         Case"h":
+        System.out.println("Parametro                mensaje");
+        break;
+        System.out.println("a                        mensaje de analisis de datos");
+        System.out.println("b                        mensaje para instalacion avanzada");
+        System.out.println("h                        mostrara toda esta lista de parametros");
+        System.out.println("e                        ejecutara el programa sin ningun elemento");
+        break;
+        Case "e":
+        System.out.println("Se ejecuto sin ningun elemento");
+        break;
+        }
+    }else{
+    	System.out.println("Estamos ejecutando el programa de forma correcta");
+    }
+}  
+ 		
