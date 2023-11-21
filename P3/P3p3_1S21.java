7c7db4759ab876f2e556ead23e8372245b3cb1b5
boodaf <144178847+boodaf@users.noreply.github.com>
2023-11-21 08:25:11 -0600

    Add files via upload

---
+++ P3/P3p3_1S21.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class P3p3_1s21{
+public static void elevacion(int a,int contador){
+	System.out.println("El valor es"+a);
+	if(!(contador==5))elevacion(a*2,contador+1);
+
+
+
+public static void main(String[] args){
+	int valor=2;
+	elevacion(2,1);
+	}
+}
